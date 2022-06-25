caaeadb0a85017ff9757d8fe2f212bd2354f75f2
Mykola Ostapchuk <kokola33258@gmail.com>
2022-06-25 16:18:11 +0200

    Update pom.xml, Book, User, Order

--- src/main/java/org/example/entity/Book.java
+++ src/main/java/org/example/entity/Book.java
@@ -1,29 +1,23 @@
-//package org.example.entity;
-//
-//import lombok.*;
-//
-//import javax.persistence.*;
-//import java.util.UUID;
-//
-//@Entity
-//@NoArgsConstructor
-//@AllArgsConstructor
-//@Builder
-//@Setter
-//@Getter
-//public class Book {
-//
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.AUTO)
-//    private UUID id;
-//
-//    private String author;
-//
-//    private String title;
-//
-//    private String description;
-//
-//    private int price;
-//
-//    private int itemCount;
-//}
+package org.example.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.UUID;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Setter
+@Getter
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    private String title;
+
+    private int price;
+}
--- src/main/java/org/example/entity/User.java
+++ src/main/java/org/example/entity/User.java
@@ -1,33 +1,33 @@
-//package org.example.entity;
-//
-//
-//import lombok.*;
-//import org.jetbrains.annotations.NotNull;
-//import org.springframework.security.core.GrantedAuthority;
-//import org.springframework.security.core.authority.SimpleGrantedAuthority;
-//import org.springframework.security.core.userdetails.UserDetails;
-//
-//import javax.persistence.*;
-//import java.util.*;
-//
-//@Entity
-//@Builder
-//@Getter
-//@Setter
-//@NoArgsConstructor
-//@AllArgsConstructor
-//public class User{
-//
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.AUTO)
-//    private UUID id;
-//
-//    @NotNull
-//    private String email;
-//
-//    @NotNull
-//    private String password;
-//
-//    @NotNull
-//    private String role;
-//}
+package org.example.entity;
+
+
+import lombok.*;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.*;
+import java.util.*;
+
+@Entity
+@Builder
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @NotNull
+    private String email;
+
+    @NotNull
+    private String password;
+
+    @NotNull
+    private String role;
+}
