db7d9a4f96bbb0ef0f56b9fb9f6988635573ac6b
OPest <j.sieczka99@wp.pl>
2022-06-26 10:36:04 +0200

    przed zmianami

--- src/main/java/org/example/controller/MainController.java
+++ src/main/java/org/example/controller/MainController.java
@@ -2,26 +2,47 @@ package org.example.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @Controller
 public class MainController {
 
-    @GetMapping
+    @RequestMapping
     String main(Model model){
         model.addAttribute("some_key","some_value");
         return "main";
     }
 
-    @GetMapping("/login")
+    @RequestMapping("/login")
     String login(Model model){
         model.addAttribute("some_key","some_value");
         return "login";
     }
 
-    @GetMapping("/register")
+    @RequestMapping("/register")
     String register(Model model){
         model.addAttribute("some_key","some_value");
         return "register";
     }
+
+    @RequestMapping("/logout")
+    String logout(){
+        return "main";
+    }
+
+    @RequestMapping("/books")
+    String books(Model model){
+        model.addAttribute("some_key","some_value");
+        return "books";
+    }
+
+    @RequestMapping("/books/{id}")
+    String book(String id, Model model){
+        model.addAttribute("some_key","some_value");
+        return "book_details";
+    }
+
+
 }
---
+++ src/main/java/org/example/model/LoginCredentials.java
@@ -0,0 +1,10 @@
+package org.example.model;
+
+
+import lombok.Getter;
+
+@Getter
+public class LoginCredentials {
+    private String username;
+    private String password;
+}
